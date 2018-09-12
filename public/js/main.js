6b33625f4dc13c5344afd4995c2b0e8861e403b8
Amit <amit@AZ-iMac.local>
2018-09-12 16:17:04 -0400

    Ghost SVG added

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,21 @@
-const btns = document.querySelectorAll(".buttons button"); //so in html go inside the ".buttons" DIV and select any "buttons".
+const btns = document.querySelectorAll("buttons button"); //so in html go inside the ".buttons" DIV and select any "buttons".
 
-const cat = document.getElementsByClassName("cat")[0];
-// Avove "cat" refers to html SVG Class="cat"
+const ghost = document.getElementClassName("ghost")[0];
+// Above "ghost" refers to html SVG Class="ghost"
 
 // Setup Event Handling for all of the buttons so that when they get clicked, something is done in response:
   // Loop throught the collection of all of the buttons using a FOR loop:
- for (i = 0; i < btns.length; i++) {
-   btns[i].addEventListener("click", manageCatClasses);
- }
+for (i = 0); i < btns; i++) {
+  btns[i].addEventListener("click", manageGhostClasses);
+}
 
   //Below create function for manageCatClasses written above:
-  function manageCatClasses() {
+  function manageGhostClasses() {
     // alert("testing 123"); //to test if buttons are working
     if (this.getAttribute("data-add")) {
-      cat.classList.add(this.getAttribute("data-add"));
-    }
+      ghost.classList.add(this.getAttribute("data-add"));
+    } // for removing:
     if (this.getAttribute("data-remove")) {
-      cat.classList.remove(this.getAttribute("data-remove"));
+      ghost.classList.remove(this.getAttribute("data-remove"));
     }
-
   }
